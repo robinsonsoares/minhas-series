f7916e19317e8ee49c5e6f0871e3ca4719e13133
Robinson Soares <soares.robinson@gmail.com>
2019-08-06 15:40:21 -0300

    Aula 2 - Rotas

---
+++ src/Generos.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Generos = () => {
+    const[data,setData] = useState([])
+    useEffect(() => {
+        axios
+        .get('/api/genres')
+        .then(res=>{
+            setData(res.data.data)
+        })
+    }, [])
+
+    const redenrizaLinha = record => {
+        return (
+            <tr key={record.id}>
+                <th scope='row'>{record.id}</th>
+                <td>{record.name}</td>
+                <td><button>+</button></td>
+            </tr> 
+        )
+    }
+    if(data.length === 0) {
+        return (
+            <div className='container'>
+                <h1>Generos</h1>
+                <div className='alert alert-warning' role='alert'>
+                    Você não possui genêros criados!
+                </div>
+            </div>
+        )
+    }
+    return (
+        <div className='container'>
+            <h1>Generos</h1>  
+            <table className='table table-dark'>
+                <thead>
+                    <tr>
+                        <th scope='col'>ID</th>
+                        <th scope='col'>Nome</th>
+                        <th scope='col'>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map(redenrizaLinha)}
+                </tbody>
+            </table>            
+        </div>
+    ) 
+}
+export default Generos
