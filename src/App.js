8dda59484b9def0dba500b9fe533ca5c525adcd1
Robinson Soares <soares.robinson@gmail.com>
2019-08-07 10:04:30 -0300

    Aula 2 - CRUD

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import Header from './Header'
 import Generos from './Generos'
-import axios from 'axios'
+import NovoGenero from './NovoGenero'
+import EditarGenero from './EditarGenero'
 import {
   BrowserRouter as Router,
-  Route
+  Route,
+  Switch
 } from 'react-router-dom'
 
 const Home = () => {
@@ -12,19 +14,16 @@ const Home = () => {
 }
 
 function App() {
-  const [data, setData] = useState({})
-  useEffect( () => {
-    axios.get('/api').then(res =>{
-      setData(res.data)
-    })
-  }, [])
   return (
     <Router>
       <div>
         <Header />
-        <Route path='/' exact component={Home} />
-        <Route path='/generos' component={Generos} />
-        <pre>{JSON.stringify(data)}</pre>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/generos' exact component={Generos} />
+          <Route path='/generos/novo' exact component={NovoGenero} />
+          <Route path='/generos/:id' exact component={EditarGenero} />          
+        </Switch>
       </div>
     </Router>
   )
--- src/Generos.js
+++ src/Generos.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 const Generos = () => {
     const[data,setData] = useState([])
@@ -11,19 +12,31 @@ const Generos = () => {
         })
     }, [])
 
+    const deleteGenero = id => {
+        axios
+            .delete('/api/genres/' + id)
+            .then(res => {
+                const filtrado = data.filter(item => item.id !== id)
+                setData(filtrado)
+            })
+    }
+
     const redenrizaLinha = record => {
         return (
             <tr key={record.id}>
                 <th scope='row'>{record.id}</th>
                 <td>{record.name}</td>
-                <td><button>+</button></td>
+                <td>
+                    <button className='btn btn-danger' onClick={() => deleteGenero(record.id)}>Remover</button>
+                    <Link to={'/generos/' + record.id} className='btn btn-warning'>Editar</Link>
+                </td>
             </tr> 
         )
     }
     if(data.length === 0) {
         return (
             <div className='container'>
-                <h1>Generos</h1>
+                <h1>Generos</h1>                
                 <div className='alert alert-warning' role='alert'>
                     Você não possui genêros criados!
                 </div>
@@ -33,6 +46,7 @@ const Generos = () => {
     return (
         <div className='container'>
             <h1>Generos</h1>  
+            <div><Link to='/generos/novo' className='btn btn-primary'>Novo genêro</Link></div>
             <table className='table table-dark'>
                 <thead>
                     <tr>
